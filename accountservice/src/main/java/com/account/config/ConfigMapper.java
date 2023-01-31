ef67e87ec54ff41a53c42fffd41b42ca673865fd
That. Nguyen Do - CMC Global DU1.26 <ndthat@cmcglobal.vn>
2023-01-31 14:35:42 +0700

    docspring

--- accountservice/src/main/java/com/account/config/ConfigMapper.java
+++ accountservice/src/main/java/com/account/config/ConfigMapper.java
@@ -4,12 +4,14 @@ import org.modelmapper.ModelMapper;
 import org.modelmapper.config.Configuration;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
 @Component
 @org.springframework.context.annotation.Configuration
 public class ConfigMapper {
 
+    @Lazy
     @Bean
     public ModelMapper modelMapper() {
         ModelMapper modelMapper = new ModelMapper();
